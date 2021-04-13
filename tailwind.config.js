03ca553c5b0666d6d2e5c6e46f689cf0acb65af0
Brett Lewis <hello@lewcrew.co>
2021-04-13 14:35:09 -0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
-    content: ['./public/**/*.html'],
+    content: ['./public/index.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Montserrat', 'sans-serif' ]
+     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#4B755C',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+    },
     extend: {},
   },
   variants: {
